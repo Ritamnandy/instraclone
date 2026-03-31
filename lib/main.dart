c28e4c4d23956ea5c9765d2f8c561f835ef92ea7
Ritamnandy <nandyritam519@gmail.com>
2026-04-01 01:43:39 +0530

    ui change

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:instra_clone/firebase_options.dart';
 import 'package:instra_clone/provider/authprovider.dart';
 import 'package:instra_clone/screens/signin_screen.dart';
 import 'package:instra_clone/view/spalhscreen.dart';
@@ -10,7 +9,7 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (context) => Authprovider())],
